1fd3a97cb0669db7364682df366c2eee097d24c3
Jake Feasel <jake.feasel@forgerock.com>
2021-06-28 16:03:05 -0700

    Adds support for ForgeRock-specific session checking
    Addresses #14

--- sessionCheck.js
+++ sessionCheck.js
@@ -5,117 +5,167 @@
      * session at the OpenID Provider is still valid and associated with the
      * same user authenticated within this app (the OIDC Relying Party).
      * @module SessionCheck
-     * @param {Object} config - configation needed for working with the OP
-     * @param {string} [config.subject] - The user currently logged into the RP. Either the subject or the idToken are required
-     * @param {string} [config.idToken] - The first id_token obtained as part of an interactive grant. Either the subject or the idToken are required
-     * @param {string} [config.responseType=id_token] - Response type to use to check the session. Supported options are id_token or none.
-     * @param {string} config.clientId - The id of this RP client within the OP
-     * @param {string} config.opUrl - Full URL to the OP Authorization Endpoint
+     * @param {Object} config - configuration needed for working with the OP
+
+     * common config options
+     * @param {string} [config.subject] - (optional) The user currently logged into the RP. Not usable with responseType=none.
      * @param {function} config.invalidSessionHandler - function to be called once any problem with the session is detected
      * @param {function} [config.initialSessionSuccessHandler] - optional function to be called after the first successful session check request.
-     * @param {function} [config.sessionClaimsHandler] - optional function to be called after every session check request. Only invoked when using responseType=id_token. Includes the claims and the count of session check requests that have been made so far.
-     * @param {string} [config.redirectUri=sessionCheck.html] - The redirect uri registered in the OP for session-checking purposes
      * @param {number} [config.cooldownPeriod=5] - Minimum time (in seconds) between requests to the opUrl
+
+     * amUrl, ssoToken and ssoTokenName are only used when using ForgeRock-specific session validation calls
+     * @param {string} [config.amUrl] - The full URL (including path to the base of the realm) of the AM server that issued the ssoToken
+     * @param {string} [config.ssoToken] - String representing the user's session within AM. Likely made available as a custom idToken claim
+     * @param {string} [config.ssoTokenName] - Name of the session token. Defaults to iPlanetDirectoryPro
+
+     * remaining options only used for standards-based session validation calls
+     * @param {string} config.opUrl - Full URL to the OP Authorization Endpoint
+     * @param {string} [config.responseType=id_token] - Response type to use to check the session. Supported options are id_token or none.
+     * @param {string} config.clientId - The id of this RP client within the OP
+     * @param {string} [config.idToken] - The first id_token obtained as part of an interactive grant. Only used with responseType=none.
+     * @param {string} [config.redirectUri=sessionCheck.html] - The redirect uri registered in the OP for session-checking purposes
      * @param {string} [config.scope=openid] - Session check scope; can be space-separated list
+     * @param {function} [config.sessionClaimsHandler] - optional function to be called after every session check request. Only invoked when using responseType=id_token. Includes the claims and the count of session check requests that have been made so far.
      */
     module.exports = function (config) {
         var calculatedUriLink;
 
-        if (!config.redirectUri) {
-            // trick used to make the DOM resolve a complete URI based on the relative URI
-            calculatedUriLink = document.createElement("a");
-            calculatedUriLink.href = "sessionCheck.html";
-
-            this.redirectUri = calculatedUriLink.href;
-        } else {
-            this.redirectUri = config.redirectUri;
-        }
         this.request_check_count = 0;
-        this.subject = config.subject;
-        this.idToken = config.idToken;
-        this.clientId = config.clientId;
-        this.opUrl = config.opUrl;
-        this.responseType = config.responseType || "id_token";
-
-        if (this.responseType === "none" && !this.idToken) {
-            throw "When using the 'none' response type, you must supply an idToken value to use as a hint when calling the OP.";
-        }
-
         this.cooldownPeriod = config.cooldownPeriod || 5;
+        this.subject = config.subject;
 
-        if (this.responseType === "id_token") {
-            this.scope = config.scope || "openid";
-        }
-
-        /*
-         * Attach a hidden iframe onto the main document body that is used to perform
-         * background OP-session checking
-         */
-        this.iframe = document.createElement("iframe");
-        this.iframe.setAttribute("id", "sessionCheckFrame" + this.clientId);
-        this.iframe.setAttribute("style", "display:none");
-        document.getElementsByTagName("body")[0].appendChild(this.iframe);
-        this.eventListenerHandle = (function (e) {
-            if (e.origin !== document.location.origin) {
-                return;
-            }
-            if (e.data.message === "sessionCheckFailed" && config.invalidSessionHandler) {
-                config.invalidSessionHandler(e.data.reason, this.request_check_count);
-            }
-
-
-            if (e.data.message === "sessionCheckSucceeded") {
-
-                // Note that "sessionCheckSucceeded" will only be triggered if using responseType=id_token
-                if (config.sessionClaimsHandler && e.data.claims) {
-                    config.sessionClaimsHandler(e.data.claims, this.request_check_count);
+        if (config.ssoToken) {
+            this.ssoTokenName = config.ssoTokenName || "iPlanetDirectoryPro";
+            this.ssoToken = config.ssoToken;
+            this.amUrl = config.amUrl + "/sessions?_action=validate";
+            this.validationHandler = (function (responseText) {
+                try {
+                    var validationResponse = JSON.parse(responseText);
+                } catch (e) {
+                    config.invalidSessionHandler(e.message, this.request_check_count);
+                    return;
                 }
-
+                if (!validationResponse.valid) {
+                    config.invalidSessionHandler("invalid_session", this.request_check_count);
+                    return;
+                }
+                if (this.subject && this.subject !== validationResponse.uid) {
+                    config.invalidSessionHandler("subject_mismatch", this.request_check_count);
+                    return;
+                }
                 if (config.initialSessionSuccessHandler && this.request_check_count === 1) {
                     config.initialSessionSuccessHandler();
                 }
-            }
-        }).bind(this);
-        window.addEventListener("message", this.eventListenerHandle);
+            }).bind(this);
+        } else { // using the standards-based prompt=none iframe approach
 
-        if (this.subject) {
-            sessionStorage.setItem("sessionCheckSubject", this.subject);
+            if (!config.redirectUri) {
+                // trick used to make the DOM resolve a complete URI based on the relative URI
+                calculatedUriLink = document.createElement("a");
+                calculatedUriLink.href = "sessionCheck.html";
+
+                this.redirectUri = calculatedUriLink.href;
+            } else {
+                this.redirectUri = config.redirectUri;
+            }
+            this.idToken = config.idToken;
+            this.clientId = config.clientId;
+            this.opUrl = config.opUrl;
+            this.responseType = config.responseType || "id_token";
+
+            if (this.responseType === "none" && !this.idToken) {
+                throw "When using the 'none' response type, you must supply an idToken value to use as a hint when calling the OP.";
+            }
+
+            if (this.responseType === "id_token") {
+                this.scope = config.scope || "openid";
+            }
+
+            /*
+             * Attach a hidden iframe onto the main document body that is used to perform
+             * background OP-session checking
+             */
+            this.iframe = document.createElement("iframe");
+            this.iframe.setAttribute("id", "sessionCheckFrame" + this.clientId);
+            this.iframe.setAttribute("style", "display:none");
+            document.getElementsByTagName("body")[0].appendChild(this.iframe);
+            this.eventListenerHandle = (function (e) {
+                if (e.origin !== document.location.origin) {
+                    return;
+                }
+                if (e.data.message === "sessionCheckFailed" && config.invalidSessionHandler) {
+                    config.invalidSessionHandler(e.data.reason, this.request_check_count);
+                }
+
+
+                if (e.data.message === "sessionCheckSucceeded") {
+
+                    // Note that claims will only be available if using responseType=id_token
+                    if (config.sessionClaimsHandler && e.data.claims) {
+                        config.sessionClaimsHandler(e.data.claims, this.request_check_count);
+                    }
+
+                    if (config.initialSessionSuccessHandler && this.request_check_count === 1) {
+                        config.initialSessionSuccessHandler();
+                    }
+                }
+            }).bind(this);
+            window.addEventListener("message", this.eventListenerHandle);
+
+            if (this.subject) {
+                sessionStorage.setItem("sessionCheckSubject", this.subject);
+            }
         }
+
         return this;
     };
 
     /**
      * Private function only to be called through the triggerSessionCheck function.
-     * Responsible for updating the hidden iframe url to trigger the implicit-flow-based
-     * id_token grant. Also constructs the nonce value, which is later verified in
-     * the sessionCheckFrame code.
+     * Responsible for starting the interaction with the identity provider, using
+     * whichever method appropriate. If using standard approach, then it will
+     * update the hidden iframe url to trigger the implicit-flow-based
+     * id_token grant and construct the nonce value, which is later verified in
+     * the sessionCheckFrame code. If using the ForgeRock approach, then it makes
+     * an XHR call to the session validation endpoint.
      */
-    var idTokenRequest = function(config) {
-        if (!config.iframe) {
-            // This session check instance has been destroyed
-            return;
-        }
-        var authorizationUrl = config.opUrl + "?prompt=none" +
-                "&client_id="     + config.clientId +
-                "&response_type=" + config.responseType +
-                "&redirect_uri="  + config.redirectUri;
+    var sessionCheckRequest = function(config) {
+        if (config.ssoToken) {
+            var req = new XMLHttpRequest();
+            req.addEventListener("load", function () {
+                config.validationHandler(this.responseText);
+            });
+            req.open("POST", config.amUrl);
+            req.setRequestHeader(config.ssoTokenName, config.ssoToken);
+            req.setRequestHeader("Accept-API-Version", "resource=2.1, protocol=1.0");
+            req.send();
+        } else {
+            if (!config.iframe) {
+                // This session check instance has been destroyed
+                return;
+            }
+            var authorizationUrl = config.opUrl + "?prompt=none" +
+                    "&client_id="     + config.clientId +
+                    "&response_type=" + config.responseType +
+                    "&redirect_uri="  + config.redirectUri;
 
-        if (config.responseType === "id_token") {
-            var nonce = Math.floor(Math.random() * 100000);
-            sessionStorage.setItem("sessionCheckNonce", nonce);
-            authorizationUrl += "&nonce=" + nonce;
-        }
+            if (config.responseType === "id_token") {
+                var nonce = Math.floor(Math.random() * 100000);
+                sessionStorage.setItem("sessionCheckNonce", nonce);
+                authorizationUrl += "&nonce=" + nonce;
+            }
 
-        if (config.scope) {
-            authorizationUrl += "&scope=" + config.scope;
-        }
+            if (config.scope) {
+                authorizationUrl += "&scope=" + config.scope;
+            }
 
-        if (config.idToken) {
-            authorizationUrl += "&id_token_hint=" + config.idToken;
-        }
+            if (config.idToken) {
+                authorizationUrl += "&id_token_hint=" + config.idToken;
+            }
 
-        config.iframe
-            .contentWindow.location.replace(authorizationUrl);
+            config.iframe
+                .contentWindow.location.replace(authorizationUrl);
+        }
     };
 
     /** @function triggerSessionCheck
@@ -124,16 +174,16 @@
      */
     module.exports.prototype.triggerSessionCheck = function () {
         // Helper function used to prevent simultaneous requests being issued
-        function idTokenRequestCooldown() {
+        function sessionCheckRequestCooldown() {
             var timestamp = (new Date()).getTime();
             if (!this.checkSessionTimestamp || (this.checkSessionTimestamp + (this.cooldownPeriod * 1000)) < timestamp) {
                 this.checkSessionTimestamp = timestamp;
                 this.request_check_count++;
-                idTokenRequest(this);
+                sessionCheckRequest(this);
             }
         }
 
-        idTokenRequestCooldown.call(this);
+        sessionCheckRequestCooldown.call(this);
     };
 
     /**
